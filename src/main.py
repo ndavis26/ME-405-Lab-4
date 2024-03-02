11884f761c2e9df64e1d8a903cdb30d75a6b18c7
ndavis26 <158105326+ndavis26@users.noreply.github.com>
2024-03-01 23:00:04 -0800

    Coding Done?
    maybe done idk

--- src/main.py
+++ src/main.py
@@ -1,13 +1,7 @@
-"""!
-@file main.py
-    This file contains a demonstration program that runs some tasks, an
-    inter-task shared variable, and a queue. The tasks don't really @b do
-    anything; the example just shows how these elements are created and run.
-
+"""! @file main.py
+Runs two motors simultaneously.
 @author Nathaniel Davis an Sebastian Bessoudo
-@date   2021-Dec-15 JRR Created from the remains of previous example
-@copyright (c) 2015-2021 by JR Ridgely and released under the GNU
-    Public License, Version 2. 
+@date   3-1-2024
 """
 
 import gc
@@ -32,80 +26,68 @@ def task1_fun():
     # set up Encoder class object
     enc1 = Encoder(pyb.Pin.board.PC6, pyb.Pin.board.PC7, 8, 1, 2)
     # set up Servo class object using the MotorDriver and Encoder objects specified
-    serv1 = Servo(moe, enc)
+    serv1 = Servo(moe1, enc1)
     
     # number of data points to run test for (each data point is 10 ms apart)
-    num = 500
     
-    # run test ad infinitum
+    # run ad infinitum
     while True:
-        # requests Kp
-        input_kp = float(input("enter Kp:"))
+        # Kp
+        input_kp = 0.05
         serv1.set_Kp(input_kp)
-        # requests setpoint
-        input_setp = int(input("enter setpoint:"))
+        # setpoint
+        input_setp = 100000
         serv1.set_setpoint(input_setp)
         
-        # create empty lists for times and positions
-        positions = []
-        times = [10*x for x in range(num)]
-        
         # repeatedly saves position value every 10 ms 
-        for n in range(num):
+        while True:
             serv1.set_setpoint(input_setp)
-            time.sleep_ms(10)
-            positions.append(serv1.encoder.read())
-            print(f"{times[n]}, {serv1.encoder.read()}")
-        # stop motor at end of test
-        serv1.run(0)
-        # print End to signal end of data transmission
-        print('End')
-        yield 
+            yield 
 
 
-#def task2_fun():
+def task2_fun():
     """!
     Task which takes things out of a queue and share and displays them.
     @param shares A tuple of a share and queue from which this task gets data
     """
-    #moe2 = MotorDriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, 1,  pyb.Pin.board.PB5, 2, 3)
-    #enc2 = Encoder(pyb.Pin.board.PB6, pyb.Pin.board.PB7, 4, 1, 2)
-    #cloop2 = Servo(moe2, enc2)
-    # Get references to the share and queue which have been passed to this task
-    #the_share, the_queue = shares
-
-    #while True:
-        # Show everything currently in the queue and the value in the share
-        #print(f"Share: {the_share.get ()}, Queue: ", end='')
-        #while q0.any():
-            #print(f"{the_queue.get ()} ", end='')
-        #print('')
-
-        #yield 0
-
+    # run motor response test
+    # set up MotorDriver class object
+    moe2 = MotorDriver(pyb.Pin.board.PC1, pyb.Pin.board.PA0, 1,  pyb.Pin.board.PA1, 2, 5)
+    # set up Encoder class object
+    enc2 = Encoder(pyb.Pin.board.PB6, pyb.Pin.board.PB7, 4, 1, 2)
+    # set up Servo class object using the MotorDriver and Encoder objects specified
+    serv2 = Servo(moe2, enc2)
+    
+    # number of data points to run test for (each data point is 10 ms apart)
+    
+    # run test ad infinitum
+    while True:
+        # Kp
+        input_kp = 0.05
+        serv2.set_Kp(input_kp)
+        # setpoint
+        input_setp = -150000
+        serv2.set_setpoint(input_setp)
+        
+        # repeatedly saves position value every 10 ms 
+        while True:
+            serv2.set_setpoint(input_setp)
+            yield 
 
 # This code creates a share, a queue, and two tasks, then starts the tasks. The
 # tasks run until somebody presses ENTER, at which time the scheduler stops and
 # printouts show diagnostic information about the tasks, share, and queue.
 if __name__ == "__main__":
-    print("Testing ME405 stuff in cotask.py and task_share.py\r\n"
-          "Press Ctrl-C to stop and show diagnostics.")
-
-    # Create a share and a queue to test function and diagnostic printouts
-    share0 = task_share.Share('h', thread_protect=False, name="Share 0")
-    q0 = task_share.Queue('L', 16, thread_protect=False, overwrite=False,
-                          name="Queue 0")
-
+    print("Press Ctrl-C to stop and show diagnostics.")
+    
     # Create the tasks. If trace is enabled for any task, memory will be
     # allocated for state transition tracing, and the application will run out
     # of memory after a while and quit. Therefore, use tracing only for 
     # debugging and set trace to False when it's not needed
-    task1 = cotask.Task(task1_fun, name="Motor 1", priority=1, period=20,
-                        profile=True, trace=False)
-    #task2 = cotask.Task(task2_fun, name="Motor 2", priority=2, period=1500,
-                        #profile=True, trace=False, shares=(share0, q0))
+    task1 = cotask.Task(task1_fun, name="Motor 1", priority=1, period=30, profile=True, trace=False)
+    task2 = cotask.Task(task2_fun, name="Motor 2", priority=2, period=30, profile=True, trace=False)
     cotask.task_list.append(task1)
-    #cotask.task_list.append(task1_motor2)
+    cotask.task_list.append(task2)
 
     # Run the memory garbage collector to ensure memory is as defragmented as
     # possible before the real-time scheduler is started
